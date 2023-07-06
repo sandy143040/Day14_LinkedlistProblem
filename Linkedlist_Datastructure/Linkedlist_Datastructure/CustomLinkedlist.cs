960d35a37faf7ff06c6af553427917205f704171
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-07-06 18:25:14 +0530

    [Sandeep] Add : Created a simple Linked list

---
+++ Linkedlist_Datastructure/Linkedlist_Datastructure/CustomLinkedlist.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linkedlist_Datastructure
+{
+    public class CustomLinkedlist
+    {
+        public void CreateList()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            list.AddLast(56);
+            list.AddLast(30);
+            list.AddLast(70);
+            foreach(int val in list)
+            {
+                Console.Write(val+"->");
+            }
+        }
+    }
+}
---
+++ Linkedlist_Datastructure/Linkedlist_Datastructure/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linkedlist_Datastructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomLinkedlist customLinkedlist = new CustomLinkedlist();
+            customLinkedlist.CreateList();
+            Console.ReadLine();
+        }
+    }
+}
